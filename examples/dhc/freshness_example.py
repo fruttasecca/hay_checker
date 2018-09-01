e7f2d0afe8b49e6d799f6c02e1545ed7b23999fa
katerynak <konotopska.k@gmail.com>
2018-09-01 20:24:08 +0200

    date format warning added

--- examples/dhc/freshness_example.py
+++ examples/dhc/freshness_example.py
@@ -9,6 +9,10 @@ df = spark.read.format("csv").option("header", "true").load("examples/resources/
 
 df.show()
 
+# be careful with dateFormat, lowercase and uppercase letters have different
+# meanings, a compete specification of simple date time format can be found
+# at https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+
 r1 = freshness(["birthDate"], dateFormat="yyyy/MM/dd", df=df)[0]
 
 print("Freshness birthDate: {}".format(r1))
