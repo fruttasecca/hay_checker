2aa7aedd6e72218babbb38476c3abaa5781830d9
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2018-09-01 11:27:05 +0200

    changes to also allow strptime time formats

--- haychecker/_common/config.py
+++ haychecker/_common/config.py
@@ -153,17 +153,7 @@ class Config(object):
         format = metric["timeFormat"] if "timeFormat" in metric else metric["dateFormat"]
         assert len(value) > 0, "Value must have length greater than 0"
         assert len(format) > 0, "Format must have length greater than 0"
-        assert len(value) == len(format), "Value %s and format %s have different length" % (value, format)
         assert any([c in Config._allowed_digits for c in format]), "Format contains no digits"
-        for i, (vchar, fchar) in enumerate(zip(value, format)):
-            if fchar in Config._allowed_digits:
-                # then vchar must be a digit
-                assert vchar.isdigit(), "Character at position %s in %s is not a digit but it should be, given the format %s" % (
-                    i, value, format)
-            else:
-                # else it's a separator like ':' for HH:MM:ss
-                assert vchar == fchar, "Character at position %s in %s should be the same at position %s in %s" % (
-                    i, value, i, format)
 
     @staticmethod
     def _deduplication_params_check(metric, error_msg):
