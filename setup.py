52c67424921d52f8edeed2abdd2d0fcf592b4f55
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2018-09-02 09:37:18 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ setup(
     name='haychecker',
     version='0.0.1',
     description='a small library to check for data quality, either with spark or pandas',
+    long_description="Description at https://github.com/fruttasecca/hay_checker",
     license='MIT',
     packages=setuptools.find_packages(),
-    author='Jacopo Gobbi and Kateryna Konotopska',
-    author_email="jacopo.gobbi@studenti.unitn.it kateryna.konotopska@studenti.unitn.it",
+    author='Jacopo Gobbi, Kateryna Konotopska',
+    author_email="jacopo.gobbi@studenti.unitn.it, kateryna.konotopska@studenti.unitn.it",
     keywords=["data quality", "completeness", "deduplication", "timeliness", "freshness", "constraint", "rule",
               "entropy", "mutual information", "spark", "pandas"],
-    url="https://github.com/fruttasecca/hay_checker"
+    url="https://github.com/fruttasecca/hay_checker",
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+        ],
 )
